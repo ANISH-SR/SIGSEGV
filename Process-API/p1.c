5189357721509ea51bc283ae061bf9177edb3f1d
ANISH-SR <rawatanish08@gmail.com>
2025-06-12 04:06:45 +0000

    init

--- Process-API/p1.c
+++ Process-API/p1.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    printf("hello (pid: %d)\n", (int) getpid());
+int
+main(int argc, char *argv[])
+{
+    printf("hello world (pid:%d)\n", (int) getpid());
     int rc = fork();
-    if(rc < 0){
-        //fork failed
+    if (rc < 0) {
+        // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
+    } else if (rc == 0) {
+        // child (new process)
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+    } else {
+        // parent goes down this path (original process)
+        printf("hello, I am parent of %d (pid:%d)\n",
+	       rc, (int) getpid());
     }
-    else if(rc == 0){
-        printf("child (pid:%d)\n", (int) getpid());
-    }
-    else{
-        printf("parent of %d (pid:%d)\n",
-        rc, (int) getpid());
-    }
-
     return 0;
 }
